d0d2142054c5e02413a61b1963fae3e972893d30
Kazuki Ota <kaota@microsoft.com>
2021-04-30 20:50:36 +0900

    プロジェクト ファイルを追加します。

---
+++ CsvHelperLab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsvHelperLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
